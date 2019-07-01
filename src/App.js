e21ee94162a91fd756450c473dc6fc6521b80597
樊亮 <1442585939@qq.com>
2019-07-01 17:33:26 +0800

    new functions

--- src/App.js
+++ src/App.js
@@ -1,91 +1,93 @@
 /**
  * Created by Administrator on 2019/6/20/020.
  */
-import React, { PropTypes, Component, useState, useEffect } from 'react';
-import { Menu, Icon, Button } from 'antd';
-import { Link } from 'dva/router';
-const { SubMenu }  = Menu;
+import React from "react";
+import { Menu, Icon, Button } from "antd";
+import { Link } from "dva/router";
+
+const { SubMenu } = Menu;
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collapsed: false,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {}
 
-    }
-    toggleCollapsed = ()=>{
-        this.setState({
-            collapsed: !this.state.collapsed,
-        });
-    }
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <div style={{ width: 256 }}>
-                    <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-                        <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
-                    </Button>
-                    <Menu
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
-                        mode="inline"
-                        theme="dark"
-                        inlineCollapsed={this.state.collapsed}
-                    >
-                        <Menu.Item key="1">
-                            <Icon type="pie-chart" />
-                            <span>USER</span>
-                            <Link to='/user'>to user page</Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="desktop" />
-                            <span>Option 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="inbox" />
-                            <span>Option 3</span>
-                        </Menu.Item>
-                        <SubMenu
-                            key="sub1"
-                            title={
-                                <span>
-                <Icon type="mail" />
-                <span>Navigation One</span>
-              </span>
-                            }
-                        >
-                            <Menu.Item key="5">Option 5</Menu.Item>
-                            <Menu.Item key="6">Option 6</Menu.Item>
-                            <Menu.Item key="7">Option 7</Menu.Item>
-                            <Menu.Item key="8">Option 8</Menu.Item>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub2"
-                            title={
-                                <span>
-                <Icon type="appstore" />
-                <span>Navigation Two</span>
-              </span>
-                            }
-                        >
-                            <Menu.Item key="9">Option 9</Menu.Item>
-                            <Menu.Item key="10">Option 10</Menu.Item>
-                            <SubMenu key="sub3" title="Submenu">
-                                <Menu.Item key="11">Option 11</Menu.Item>
-                                <Menu.Item key="12">Option 12</Menu.Item>
-                            </SubMenu>
-                        </SubMenu>
-                    </Menu>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div style={{ width: 256 }}>
+          <Button
+            type="primary"
+            onClick={this.toggleCollapsed}
+            style={{ marginBottom: 16 }}
+          >
+            <Icon type={this.state.collapsed ? "menu-unfold" : "menu-fold"} />
+          </Button>
+          <Menu
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+            theme="dark"
+            inlineCollapsed={this.state.collapsed}
+          >
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>用户</span>
+              <Link to="/user">to user page</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="inbox" />
+              <span>Option 3</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="mail" />
+                  <span>Navigation One</span>
+                </span>
+              }
+            >
+              <Menu.Item key="5">Option 5</Menu.Item>
+              <Menu.Item key="6">Option 6</Menu.Item>
+              <Menu.Item key="7">Option 7</Menu.Item>
+              <Menu.Item key="8">Option 8</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="appstore" />
+                  <span>Navigation Two</span>
+                </span>
+              }
+            >
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="11">Option 11</Menu.Item>
+                <Menu.Item key="12">Option 12</Menu.Item>
+              </SubMenu>
+            </SubMenu>
+          </Menu>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-export default App
+export default App;
