73a0f3183084c6917310f7ba82b71a414c3a1d31
樊亮 <1442585939@qq.com>
2019-07-08 16:21:59 +0800

    add menu

--- src/components/layout/basicLayout.js
+++ src/components/layout/basicLayout.js
@@ -2,11 +2,15 @@
  * Created by Administrator on 2019/6/20/020.
  */
 import React,{PureComponent} from "react";
-import { Layout, Menu, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import { Layout, Menu, Form, Input,form,Icon  } from 'antd';
+const { Header, Content, Footer, Sider, } = Layout;
 import { Link } from "dva/router";
 import { Route } from 'dva/router';
-
+import './layout.less';
+import logo from '../../imgs/logo-head.png'
+import user from '../../imgs/user.png'
+const { SubMenu } = Menu;
+const { Search } = Input;
 const userMenu = [
     {
         "code": "counter",
@@ -33,17 +37,21 @@ const userMenu = [
         ]
     },
 ];
+@Form.create()
 export default class BasicLayout extends PureComponent {
     constructor(props) {
         super(props);
+        this.state = {
+        };
+
     }
 
     componentDidMount() {
     }
 
-
     render() {
         const { navData } = this.props;
+        const { getFieldDecorator } = form;
         return (
             <Layout>
                 <Sider
@@ -54,19 +62,61 @@ export default class BasicLayout extends PureComponent {
                         left: 0,
                     }}
                 >
-                    <div className="logo" />
+                    <div className="layout-logo">
+                        <img src={logo} alt="logo"/>
+                        <h1>AC-DR</h1>
+                    </div>
+
+
+                    <div className="layout-search">
+                        <Search placeholder="input search text" onSearch={value => console.log(value)} enterButton />
+                    </div>
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
                         {userMenu.map(item => {
                             return (
-                                <Menu.Item key={`${item.name}`}>
-                                    <Link to={{ pathname: `/${item.code}` }}>
+                                <Menu.Item key={`${item.name}`}  to={{ pathname: `/${item.code}` }}>
+                                    <span>
+                                        <Link to={{ pathname: `/${item.code}` }}>
                                         {item.name}
                                     </Link>
+                                    </span>
                                 </Menu.Item>
                             );
                         })}
+
+                        <SubMenu
+                            key="sub1"
+                            title={
+                                <span>
+                                     <Icon type="mail" />
+                                     <span>Navigation One</span>
+                                 </span>
+                            }
+                        >
+                            <Menu.Item key="5">
+                                <Link to={{ pathname: `/counter` }}>
+                                    counter
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="6">
+                                <Link to={{ pathname: `/password` }}>
+                                    password
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="7">Option 7</Menu.Item>
+                            <Menu.Item key="8">Option 8</Menu.Item>
+                        </SubMenu>
                     </Menu>
+
+
+                    <div className="layout-user">
+                        <img src={user} alt="用户"/>
+                        <h1>张三</h1>
+                        <a href="#">logout</a>
+                    </div>
+
                 </Sider>
+
                 <Layout style={{ marginLeft: 200 }}>
                     <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
                         {navData.map((item, index) => (
